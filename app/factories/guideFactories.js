edb81c9a9425ef973b84fb453312573ed44f5c41
j-west <westja89@gmail.com>
2017-01-18 14:36:39 -0600

    Test

--- app/factories/guideFactories.js
+++ app/factories/guideFactories.js
@@ -1,8 +1,11 @@
-app.factory("GuideFactory", function($http, $q) {
+app.factory("GuideFactory", function($http) {
 
   function getGuides() {
        return $http.get(`../../data/guides.json`)
   };
 
   return {getGuides}
-})
+});
+
+
+
