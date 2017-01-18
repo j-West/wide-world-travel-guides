4dcf5a8f34bf87aca76f3bc42764e62774e1a10e
j-west <westja89@gmail.com>
2017-01-18 14:21:54 -0600

    Refactor  usage with

--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -2,10 +2,8 @@ app.controller("BookCtrl", function($scope, $http, GuideFactory) {
   console.log("BookCtrl")
 
 GuideFactory.getGuides().then(function (data) {
-  $scope.guides = data;
+  $scope.guides = data.data.guides;
   console.log(data.data.guides);
   })
-
-
 
 });
--- app/factories/guideFactories.js
+++ app/factories/guideFactories.js
@@ -1,18 +1,8 @@
 app.factory("GuideFactory", function($http, $q) {
 
   function getGuides() {
-
-    return $q(function (resolve, reject) {
-
-      $http.get(`../../data/guides.json`)
-
-           .then(function(data) {
-             resolve(data)
-           });
-            //$scope.list.push("another thing!")
-
-  })
-  }
+       return $http.get(`../../data/guides.json`)
+  };
 
   return {getGuides}
 })
